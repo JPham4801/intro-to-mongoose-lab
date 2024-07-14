d63ce85669fcba88b78a2ee27ab389b74ff723b0
JPham4801 <JPham4801@gmail.com>
2024-07-14 05:05:36 -0500

    WIP update customer

--- app.js
+++ app.js
@@ -1,51 +1,125 @@
+/*-------------------------------- Starter Code --------------------------------*/
+const dotenv = require('dotenv');
+dotenv.config();
+const Customer = require('./models/customer.js');
+const mongoose = require('mongoose');
+
+const connect = async () => {
+  await mongoose.connect(process.env.MONGODB_URI);
+  console.log('Connected to MongoDB');
+};
+
+const disconnect = async () =>{
+  mongoose.connection.close();
+  console.log('Disconnected from MongoDB');
+  process.exit();
+}
+
+/*-------------------------------- Query Functions --------------------------------*/
 const prompt = require('prompt-sync')({sigint: true});
-const lineBreak = () => console.log("---------------------------------------");
+const lineBreak = () => console.log(`
+-----------------------------------------------
+  `);
+let isActiveApp = false;
 
 const init = () =>{
-  // console.log("Welcome to the CRM");
-  // console.log("Press 'CTRL + C' at anytime to quit");
-  // prompt(`Press 'ENTER' to continue `);
-  // lineBreak();
-  menuOptionsPrompt();
-}
-
-const menuOptionsPrompt = () =>{
-  console.log(`
-  What would you like to do?
-      
-  1. Create a customer
-  2. View all customers
-  3. Update a customer
-  4. Delete a customer
-  5. Quit
-  `);
-  const menuOptionNum = prompt(`Select a number option: `);
+  lineBreak()
+  console.log("Welcome to the CRM");
+  prompt(`Press 'ENTER' to connect.. `);
+  isActiveApp = true;
   lineBreak();
-  menuOptionsHandler(menuOptionNum)
+  mainMenu();
 }
 
-const menuOptionsHandler = (optionNum) =>{
-  switch (optionNum){
+const mainMenu = async () =>{
+  await connect();
+  while(isActiveApp){
+    lineBreak();
+    console.log(`
+    What would you like to do?
+      
+    1. Create a customer
+    2. View all customers
+    3. Update a customer
+    4. Delete a customer
+    5. Quit
+  `);
+  const userSelection = prompt(`Enter a number option: `);
+  lineBreak();
+
+  switch (userSelection){
     case '1':
-      console.log(`1. Create a customer`);
+      await createCustomer();
       break;
     case '2':
-      console.log(`2. View all customers`);
+      await viewAllCustomer();
       break;
     case '3':
-      console.log(`3. Update a customer`);
+      await updateCustomer();
       break;
     case '4':
-      console.log(`4. Delete a customer`);
+      await deleteCustomer();
       break;
     case '5':
-      console.log(`Quitting... `);
+      await quitApp();
       break;
     default:
-      prompt("That option was not valid. Press 'ENTER' to return ");
+      prompt("That option is not valid. Press 'ENTER' to return to try again ");
       lineBreak();
-      menuOptionsPrompt();
+      mainMenu();
   }
+  }
+}
+
+const createCustomer = async () => {
+  console.log(`
+    1. Create a customer
+    `);
+  const customerName = prompt('What is the name of the customer? ')
+  const customerAge = prompt("What is the customer's age? ")
+  lineBreak();
+  const customerData = {
+    name: customerName,
+    age: customerAge
+  }
+  
+  const customer = await Customer.create(customerData)
+  console.log('New customer: ', customer);
+}
+
+const viewAllCustomer = async () => {
+  console.log(`
+    2. View all customers
+    `);
+  const customers = await Customer.find({});
+  console.log('All customers: ', customers);
+}
+
+const updateCustomer = async () => {
+  console.log(`
+    3. Update a customer
+    `);
+  console.log(`
+    Below is a list of customers:
+    `);
+  const customers = await Customer.find({});
+  console.log('All customers: ', customers);
+  lineBreak()
+  const customerId = prompt(`Copy and paste the id of the customer you would like to update here: `);
+
+  const newName = Prompt(`What is the customer's new name? `);
+  const newAge = Prompt(`What is the customer's new age? `);
+}
+
+const deleteCustomer = async () => {
+  console.log(`
+    4. Delete a customer
+    `);
+}
+
+const quitApp = async () => {
+  isActiveApp = false;
+  await disconnect();
 }
 
 init();
--- models/customer.js
+++ models/customer.js
@@ -3,10 +3,8 @@ const mongoose = require('mongoose');
 // Define customer scheme
 const customerSchema = new mongoose.Schema({
   name: String,
-  age: Number,
+  age: Number
 })
 
 // Compile the schema into a model
-const Customer = mongoose.model('Customer', customerSchema)
-
-module.exports = Customer;
+module.exports = mongoose.model('Customer', customerSchema)
